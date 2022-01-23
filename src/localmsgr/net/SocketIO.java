bff012e24accb78ef98fcc7cf42d2ad646cb0c35
410 <410dev.s@gmail.com>
2022-01-23 05:25:08 -0500

    Show message if port is already in use

--- src/localmsgr/net/SocketIO.java
+++ src/localmsgr/net/SocketIO.java
@@ -45,7 +45,8 @@ public class SocketIO {
                         }
                     }
                 }catch(Exception e){
-                    SystemLogger.error("Error while waiting for input", false, SystemLogger.CONTINUE, e);
+                    if (e.toString().contains("Address already in use")) {
+                        SystemLogger.error("Port " + Config.recvPort + " is already in use. Please close the program using such port and try again.", true, SystemLogger.EXIT, e);
                 }
             }
         };
